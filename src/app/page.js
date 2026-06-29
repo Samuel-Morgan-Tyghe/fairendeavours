cd2b53bb78128fec9c36992c098b837490102705
Sam Morgan-Tyghe <samuelmorganisual@gmail.com>
2026-06-29 22:14:15 +0100

    Fix logo path and make basePath env-driven

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
 import {FacebookLogoIcon, InstagramLogoIcon} from "@phosphor-icons/react/ssr";
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export default function Home() {
   return (
       <div className={'flex flex-col min-h-screen flex-1 items-center justify-center p-12'}>
         <div className={'lg:max-w-[33%] md:max-w-[60%]'}>
-            <Image src={'/logo.png'} alt={'Logo'} width={1000} height={200} />
+            <img src={`${basePath}/logo.png`} alt={'Logo'} className={'w-full h-auto'} />
             <p className={'font-serif italic mt-24 lg:mt-36 text-sm'}>Purveyors of fine live music events across the UK since 2013</p>
             <div className={'mt-7 flex space-x-4 items-center'}>
                 <Link href={'https://linktr.ee/fairendeavours'} target={'_blank'} className={'font-bold uppercase tracking-widest text-sm'}>
